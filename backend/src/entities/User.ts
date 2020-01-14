04f0487098964dbe5177c9d2512b476a051bc7f0
jmkim <darkkjm2002@naver.com>
2020-01-14 19:32:33 +0900

    Developing for login with facebook.

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,11 +1,16 @@
-import bcrypt from 'bcrypt';
-import { IsEmail } from 'class-validator';
+import bcrypt from "bcrypt";
+import { IsEmail } from "class-validator";
 import {
-  BaseEntity, BeforeInsert, BeforeUpdate,
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
-  Entity, ManyToOne, OneToMany,
-  PrimaryGeneratedColumn, UpdateDateColumn
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
 } from "typeorm";
 import Chat from "./Chat";
 import Message from "./Message";
@@ -18,66 +23,84 @@ const BCRYPT_ROUNDS = 10;
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({type: "text", unique: true})
+  @Column({ type: "text", nullable: true })
   @IsEmail()
-  email: string;
+  email: string | null;
 
-  @Column({type: "boolean", default: false})
+  @Column({ type: "boolean", default: false })
   verifiedEmail: boolean;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   firstName: string;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   lastName: string;
 
-  @Column({type: "int"})
+  @Column({ type: "int", nullable: true })
   age: number;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   password: string;
 
-  @Column({type: "text"})
+  @Column({ type: "text" })
   phoneNumber: string;
 
-  @Column({type: "boolean", default: false})
+  @Column({ type: "boolean", default: false })
   verifiedPhoneNumber: boolean;
 
-  @Column({type: "text"})
+  @Column({ type: "text", nullable: true })
+  fbId: string;
+
+  @Column({ type: "text" })
   profilePhoto: string;
 
-  @Column({type: "boolean", default: false})
+  @Column({ type: "boolean", default: false })
   isDriving: boolean;
 
-  @Column({type: "boolean", default: false})
+  @Column({ type: "boolean", default: false })
   isRiding: boolean;
 
-  @Column({type: "boolean", default: false})
+  @Column({ type: "boolean", default: false })
   isTaken: boolean;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   lastLng: number;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   lastLat: number;
 
-  @Column({type: "double precision", default: 0})
+  @Column({ type: "double precision", default: 0 })
   lastOrientation: number;
 
-  @ManyToOne(type => Chat, chat => chat.participants)
+  @ManyToOne(
+    type => Chat,
+    chat => chat.participants
+  )
   chat: Chat;
 
-  @OneToMany(type => Message, message => message.user)
+  @OneToMany(
+    type => Message,
+    message => message.user
+  )
   messages: Message[];
 
-  @OneToMany(type => Verification, verification => verification.user)
+  @OneToMany(
+    type => Verification,
+    verification => verification.user
+  )
   verifications: Verification[];
 
-  @OneToMany(type => Ride, ride => ride.passenger)
-  ridesAsPassenger : Ride[];
+  @OneToMany(
+    type => Ride,
+    ride => ride.passenger
+  )
+  ridesAsPassenger: Ride[];
 
-  @OneToMany(type => Ride, ride => ride.driver)
-  ridesAsDriver : Ride[];
+  @OneToMany(
+    type => Ride,
+    ride => ride.driver
+  )
+  ridesAsDriver: Ride[];
 
   @CreateDateColumn() createAt: string;
   @UpdateDateColumn() updateAt: string;
@@ -101,7 +124,6 @@ class User extends BaseEntity {
   private hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, BCRYPT_ROUNDS);
   }
-
 }
 
 export default User;
--- backend/src/types/graph.d.ts
+++ backend/src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  distance: String!\n  duration: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String\n  verrifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  verifiedEmail: Boolean!\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createAt: String!\n  updateAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createAt: String!\n  updateAt: String\n}\n"];
+export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]\n  participants: [User]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: [Chat]!\n  user: [User]!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  distance: String!\n  duration: String!\n  dirver: User!\n  passenger: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype FacebookConnectResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  FacebookConnect(firstName: String!, lastName: String!, email: String, fbId: String!): FacebookConnectResponse!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verrifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  verifiedEmail: Boolean!\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  chat: Chat\n  fbId: String\n  messages: [Message]\n  verifications: [Verification]\n  ridesAsPassenger: [Ride]\n  ridesAsDriver: [Ride]\n  createAt: String!\n  updateAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  user: User!\n  createAt: String!\n  updateAt: String\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -17,6 +17,12 @@ export interface User {
   phoneNumber: string | null;
   verifiedPhoneNumber: boolean;
   profilePhoto: string | null;
+  chat: Chat | null;
+  fbId: string | null;
+  messages: Array<Message> | null;
+  verifications: Array<Verification> | null;
+  ridesAsPassenger: Array<Ride> | null;
+  ridesAsDriver: Array<Ride> | null;
   createAt: string;
   updateAt: string | null;
   fullName: string | null;
@@ -28,15 +34,32 @@ export interface User {
   lastOrientation: number | null;
 }
 
-export interface Place {
+export interface Chat {
   id: number;
-  name: string;
-  lat: number;
-  lng: number;
-  address: string;
-  isFav: boolean;
+  messages: Array<Message> | null;
+  participants: Array<User> | null;
   createdAt: string;
   updatedAt: string | null;
+}
+
+export interface Message {
+  id: number;
+  text: string;
+  chat: Array<Chat>;
+  user: Array<User>;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Verification {
+  id: number;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  user: User;
+  createAt: string;
+  updateAt: string | null;
 }
 
 export interface Ride {
@@ -51,16 +74,36 @@ export interface Ride {
   price: number;
   distance: string;
   duration: string;
+  dirver: User;
+  passenger: User;
   createdAt: string;
   updatedAt: string | null;
 }
 
-export interface Verification {
+export interface Mutation {
+  FacebookConnect: FacebookConnectResponse;
+}
+
+export interface FacebookConnectMutationArgs {
+  firstName: string;
+  lastName: string;
+  email: string | null;
+  fbId: string;
+}
+
+export interface FacebookConnectResponse {
+  ok: boolean;
+  error: string | null;
+  token: string | null;
+}
+
+export interface Place {
   id: number;
-  target: string;
-  payload: string;
-  key: string;
-  used: boolean;
-  createAt: string;
-  updateAt: string | null;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  isFav: boolean;
+  createdAt: string;
+  updatedAt: string | null;
 }
