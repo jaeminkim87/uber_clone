dc1962d9c6ef438913cec1fe7d9a92b62e58896d
jaemin kim <darkkjm2002@gmail.com>
2020-01-20 16:52:37 +0900

    Add decodeJWT

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,47 @@
-import {GraphQLServer} from "graphql-yoga";
-import cors from 'cors';
-import helmet from 'helmet';
-import logger from 'morgan';
+import cors from "cors";
+import { NextFunction, Request, Response } from "express";
+import { GraphQLServer } from "graphql-yoga";
+import helmet from "helmet";
+import logger from "morgan";
 import schema from "./schema";
+import decodeJWT from "./utils/decodeJWT";
 
 class App {
   public app: GraphQLServer;
   constructor() {
     this.app = new GraphQLServer({
-      schema
+      schema,
+      context: req => {
+        return {
+          req: req.request
+        };
+      }
     });
     this.middleWares();
   }
-  private middleWares = () : void => {
+  private middleWares = (): void => {
     this.app.express.use(cors());
     this.app.express.use(logger("dev"));
     this.app.express.use(helmet());
-  }
+    this.app.express.use(this.jwt);
+  };
+
+  private jwt = async (
+    request,
+    response: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const token = request.get("X-JWT");
+    if (token) {
+      const user = await decodeJWT(token);
+      if (user) {
+        request.user = user;
+      } else {
+        request.user = undefined;
+      }
+    }
+    next();
+  };
 }
 
 export default new App().app;
